0a68381f0c5bc9be25cbce90e01cd77e209230a1
sotiris-rafail <smoschop@gmail.com>
2019-03-17 17:07:25 +0200

    Fix

--- src/app/auction/item-service.service.ts
+++ src/app/auction/item-service.service.ts
@@ -3,9 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ItemService {
   headers: HttpHeaders;
 
